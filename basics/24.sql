c43b6badf2bf7b5e4830a9f4f6655e766ca86fe7
gerenium <75989297+gerenium@users.noreply.github.com>
2021-04-09 02:00:40 +0300

    Create 24.sql

---
+++ basics/24.sql
@@ -0,0 +1,12 @@
+INSERT INTO fine VALUES
+        (null, "Баранов П.Е.", "Р523ВТ", "Превышение скорости(от 40 до 60)", null, '2020-02-14', null),
+        (null, 'Абрамова К.А.','О111АВ','Проезд на запрещающий сигнал',null,'2020-02-23',null),
+        (null, 'Яковлев Г.Р.', 'Т330ТТ', 'Проезд на запрещающий сигнал', null,'2020-03-03',null);
+        
+SELECT name, 
+        number_plate,
+        violation,
+        sum_fine,
+        date_violation,
+        date_payment
+FROM fine
