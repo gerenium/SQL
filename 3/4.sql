48fe1d6deaca4ed3c0ccb3c90afbfb49b055a32a
gerenium <75989297+gerenium@users.noreply.github.com>
2021-04-09 02:17:51 +0300

    Create 4.sql

---
+++ 3/4.sql
@@ -0,0 +1,11 @@
+SELECT buy.buy_id, 
+      client.name_client, 
+      SUM(book.price * buy_book.amount) AS Стоимость
+FROM client JOIN buy
+            USING(client_id)
+                    JOIN buy_book
+                    USING(buy_id)
+                            JOIN book
+                            USING(book_id)
+GROUP BY buy_id, name_client
+ORDER BY buy_id
