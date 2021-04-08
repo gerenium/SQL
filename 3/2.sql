3e21a85f78f51390052fa99511fae7590b71011e
gerenium <75989297+gerenium@users.noreply.github.com>
2021-04-09 02:15:46 +0300

    Update 2.sql

--- 3/2.sql
+++ 3/2.sql
@@ -1,13 +1,20 @@
 UPDATE fine,
        payment
 SET fine.date_payment=payment.date_payment
-WHERE fine.name=payment.name AND fine.number_plate = payment.number_plate AND fine.violation=payment.violation AND fine.date_violation = payment.date_violation;
+WHERE fine.name=payment.name 
+       AND fine.number_plate = payment.number_plate 
+       AND fine.violation=payment.violation 
+       AND fine.date_violation = payment.date_violation;
 
 
 UPDATE fine,
        payment
 SET fine.sum_fine = fine.sum_fine/2
-WHERE DATEDIFF(fine.date_payment, fine.date_violation)+1<=20 AND fine.name=payment.name AND fine.number_plate = payment.number_plate AND fine.violation=payment.violation AND fine.date_violation = payment.date_violation;
+WHERE DATEDIFF(fine.date_payment, fine.date_violation)+1<=20 
+       AND fine.name=payment.name 
+       AND fine.number_plate = payment.number_plate 
+       AND fine.violation=payment.violation 
+       AND fine.date_violation = payment.date_violation;
 
 
 SELECT name,
