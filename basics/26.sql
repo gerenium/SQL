631520a1afc3efdf2f28a5b9f01558704e05b682
gerenium <75989297+gerenium@users.noreply.github.com>
2021-04-09 02:03:40 +0300

    Create 26.sql

---
+++ basics/26.sql
@@ -0,0 +1,8 @@
+Create table back_payment AS
+SELECT name,
+               number_plate,
+               violation,
+               sum_fine,
+               date_violation
+        FROM fine
+        WHERE date_payment IS null
