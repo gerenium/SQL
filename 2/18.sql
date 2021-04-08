58a0cb2695373378d8e408a0461c42f5b91e23cd
gerenium <75989297+gerenium@users.noreply.github.com>
2021-04-09 02:11:07 +0300

    Update 18.sql

--- 2/18.sql
+++ 2/18.sql
@@ -1,7 +1,14 @@
-SELECT name_author
-FROM book JOIN author
-ON book.author_id = author.author_id
-        JOIN genre
-        ON genre.genre_id = book.genre_id
-GROUP BY name_author
-HAVING count(DISTINCT name_genre) = 1
+UPDATE book JOIN author
+        USING(author_id), supply 
+SET book.price = (book.price*book.amount + supply.price*supply.amount)/(supply.amount+book.amount),
+    book.amount = book.amount + supply.amount,
+    supply.amount = 0
+WHERE book.price <> supply.price 
+        AND book.title = supply.title 
+        AND supply.author = author.name_author;
+
+select *
+from book;
+
+select *
+from supply
