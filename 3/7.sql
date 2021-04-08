d6ded3ea7a7eba5edfeed86e7a9c6998f406dea6
gerenium <75989297+gerenium@users.noreply.github.com>
2021-04-09 02:22:19 +0300

    Update 7.sql

--- 3/7.sql
+++ 3/7.sql
@@ -5,13 +5,16 @@ FROM
     buy_archive
 GROUP BY YEAR(date_payment), MONTHNAME(date_payment) 
 UNION
-SELECT  YEAR(date_step_end) AS Год, MONTHNAME(date_step_end) Месяц, sum(price * buy_book.amount) Сумма
+SELECT  YEAR(date_step_end) AS Год, 
+        MONTHNAME(date_step_end) Месяц, 
+        sum(price * buy_book.amount) Сумма
 FROM 
     book 
      JOIN buy_book USING(book_id)
      JOIN buy USING(buy_id) 
      JOIN buy_step USING(buy_id)
      JOIN step USING(step_id)  
-WHERE  date_step_end IS NOT Null and name_step = "Оплата"
+WHERE  date_step_end IS NOT Null
+        AND name_step = "Оплата"
 GROUP BY YEAR(date_step_end), MONTHNAME(date_step_end) 
 ORDER BY Месяц
